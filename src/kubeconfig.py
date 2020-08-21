4240e208d5d412eb98e26259596512fc7a4cf6e3
Allan Taylor <allan@startmail.com>
2020-08-21 11:20:36 -0400

    WIP

--- src/kubeconfig.py
+++ src/kubeconfig.py
@@ -9,7 +9,8 @@ import pathlib
 
 class KubeConfig():
 
-    config_path = "/home/scm/.kube/config"
+    home_path = str(pathlib.Path.home())
+    config_path = os.path.join(home_path, ".kube", "config")
 
     def __init__(self):
         self.config_path = KubeConfig.config_path
@@ -19,34 +20,76 @@ class KubeConfig():
         self.cluster_name = data["clusters"][0]["name"]
         self.current_context = data["contexts"][0]["name"]
 
-    def _k8sCoreApi(self):
-        kubernetes.config.load_kube_config(config_file=self.config_path)
-        return kubernetes.client.ApiClient()
+    def _k8sApi(self):
+        try:
+            kubernetes.config.load_kube_config(config_file=self.config_path)
+            return kubernetes.client.ApiClient()
+        except Exception as e:
+            logging.error(e)
+
+    def _k8sCoreV1Api(self):
+        try:
+            configuration = kubernetes.client.Configuration(config_file=self.config_path)
+            return kubernetes.client.CoreV1Api()
+        except Exception as e:
+            logging.error(e)
+
+    def _getNamespaceStatus(self, namespace):
+        kubernetes.config.load_kube_config()
+        v1 = kubernetes.client.CoreV1Api()
+        request =  v1.read_namespace(namespace)
+        return request.status
 
     def _createNameSpace(self, namespace):
         try:
-            with open("templates/namespace.yaml", "r") as file:
-                data = file.read()
-                data = data.replace("##namespace##", namespace)
-            with open("namespace.yaml", "w") as file:
-                file.write(data)
-            client = KubeConfig._k8sCoreApi(self)
-            request = kubernetes.utils.create_from_yaml(client, "namespace.yaml")
-            return request
+            kubernetes.config.load_kube_config()
+            v1 = kubernetes.client.CoreV1Api()
+            ns = kubernetes.client.V1Namespace()
+            ns.metadata = kubernetes.client.V1ObjectMeta()
+            ns.metadata.name = namespace
+            return v1.create_namespace(ns)
+        except Exception as e:
+            logging.error(e)
+
+    def _deleteNameSpace(self, namespace):
+        try:
+            kubernetes.config.load_kube_config()
+            v1 = kubernetes.client.CoreV1Api()
+            return v1.delete_namespace(name=namespace)
         except Exception as e:
             logging.error(e)
 
     def _createServiceAccount(self, namespace):
         try:
-            sa_name = namespace + "-service-account"
-            with open("templates/service-account.yaml", "r") as file:
-                data = file.read()
-                data = data.replace("##namespace##", namespace)
-            with open("service-account.yaml", "w") as file:
-                file.write(data)
-            client = KubeConfig._k8sCoreApi(self)
-            request = kubernetes.utils.create_from_yaml(client, "service-account.yaml")
-            return sa_name
+            kubernetes.config.load_kube_config()
+            v1 = kubernetes.client.CoreV1Api()
+            service_account = kubernetes.client.V1ServiceAccount()
+            service_account.metadata = kubernetes.client.V1ObjectMeta()
+            service_account.metadata.name = (namespace + "-service-account")
+            return v1.create_namespaced_service_account(namespace, service_account)
+        except Exception as e:
+            logging.error(e)
+
+    def _deleteServiceAccount(self, namespace):
+        try:
+            kubernetes.config.load_kube_config()
+            v1 = kubernetes.client.CoreV1Api()
+            service_account = (namespace + "-service-account")
+            return v1.delete_namespaced_service_account(service_account, namespace)
+        except Exception as e:
+            logging.error(e)
+
+    def _getServiceAccountToken(self, namespace):
+        try:
+            kubernetes.config.load_kube_config(config_file=self.config_path)
+            v1 = kubernetes.client.CoreV1Api()
+            name = (namespace + "-service-account")
+            sa = v1.read_namespaced_service_account(name=name, namespace=namespace)
+            secret_name = sa.secrets[0].name
+            secret = v1.read_namespaced_secret(secret_name, namespace)
+            encoded_token = secret.data["token"]
+            decoded_token = base64.b64decode(encoded_token)
+            return str(decoded_token, "utf-8")
         except Exception as e:
             logging.error(e)
 
@@ -63,21 +106,13 @@ class KubeConfig():
         except Exception as e:
             logging.error(e)
 
-    def _getServiceAccountToken(self, sa, namespace):
-        try:
-            kubernetes.config.load_kube_config(config_file=self.config_path)
-            client = kubernetes.client.CoreV1Api()
-            request = client.read_namespaced_service_account(name=sa, namespace=namespace)
-            secret_name = request.secrets[0].name
-            request = client.read_namespaced_secret(secret_name, namespace)
-            encoded_token = request.data["token"]
-            decoded_token = base64.b64decode(encoded_token)
-            return str(decoded_token, "utf-8")
-        except Exception as e:
-            logging.error(e)
+    def _readYaml(self, file):
+        with open(file, 'r') as f:
+            config_yaml = yaml.load(f)
+        return config_yaml
 
     def _setKubeConfigContext(self, config_path, namespace, token):
-        starting_context = "demo-build-cluster-01"
+        starting_context = self.current_context
         subprocess.call(["kubectl", "--kubeconfig", config_path, "config", "use-context", starting_context])
         subprocess.call(["kubectl", "--kubeconfig", config_path, "config", "view", "--flatten", "--minify"])
         subprocess.call(["kubectl", "config", "--kubeconfig", config_path, "rename-context", starting_context, namespace])
---
+++ tests/test_kubeconfig.py
@@ -0,0 +1,50 @@
+import unittest
+import os
+import time
+import src.kubeconfig
+
+class KubeConfigTest(unittest.TestCase):
+
+    namespace = "test"
+
+    def setUp(self):
+        self.kc = src.kubeconfig.KubeConfig()
+        self.kc._createNameSpace(KubeConfigTest.namespace)
+
+    def tearDown(self):
+        try:
+            self.kc._deleteNameSpace(KubeConfigTest.namespace)
+            while self.kc._getNamespaceStatus(KubeConfigTest.namespace) != None:
+                time.sleep(1)
+        except:
+            print("Namespace no longer found")
+
+    def test_config_path(self):
+        path = os.path.isfile(self.kc.config_path)
+        self.assertTrue(path)
+
+    def test_api_var_not_empty(self):
+        api = self.kc.api_server
+        self.assertRegex(api, r'^https://([a-z0-9_\.-]+):6443')
+
+    def test_cluster_name_var_not_empty(self):
+        name = self.kc.cluster_name
+        self.assertTrue(name)
+
+    def test_cluster_context_var_not_empty(self):
+        name = self.kc.current_context
+        self.assertTrue(name)
+
+    def test_create_service_account(self):
+        sa = self.kc._createServiceAccount(KubeConfigTest.namespace)
+        self.assertRegex(str(sa.metadata), r'-service-account')
+
+    def test_get_service_account_token(self):	
+        self.kc._createServiceAccount(KubeConfigTest.namespace)
+        token = self.kc._getServiceAccountToken(KubeConfigTest.namespace)
+        self.assertTrue(len(token) > 100)
+
+    def test_delete_service_account(self):
+        self.kc._createServiceAccount(KubeConfigTest.namespace)
+        sa = self.kc._deleteServiceAccount(KubeConfigTest.namespace)
+        self.assertRegex(str(sa.metadata.resource_version), r'([0-9])')
